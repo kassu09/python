1306474465096d7c662330c799f956272f8ec35f
Kaspar Põlluste <kpolluste@hkhk.edu.ee>
2026-02-18 13:53:01 +0200

    Update iseseisev töö.py

--- iseseisev töö.py
+++ iseseisev töö.py
@@ -1,28 +1,28 @@
 import turtle
 
-def edasi_parem(pikkus, nurk):
-    turtle.forward(pikkus)
-    turtle.right(nurk)
-
-def edasi_vasak(pikkus, nurk):
-    turtle.forward(pikkus)
-    turtle.left(nurk)
-
+import turtle
 
 def kujund3():
     for i in range(4):
-        edasi_parem(150, 90)
+        turtle.forward(150)
+        turtle.right(90)
+        
     turtle.forward(75)
     turtle.right(90)
     turtle.forward(40)
     turtle.left(90)
+    
     for i in range(3):
-        edasi_vasak(150, 90)
+        turtle.forward(150)
+        turtle.left(90)
+        
     turtle.forward(110)
 
 def kujund5():
     for i in range(6):
-        edasi_vasak(100, 60)
+        turtle.forward(100)
+        turtle.left(60)
+        
     turtle.left(60)
     turtle.forward(200)
     turtle.left(180)
